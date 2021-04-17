1e162124d1c333d50da587507467b3614522426f
jgtorrejon <joel.torrejon.mendez@gmail.com>
2021-04-16 21:01:12 -0400

    easy: add challenge # 1

---
+++ easy/challenge-1.js
@@ -0,0 +1,12 @@
+// Challenge link: https://edabit.com/challenge/vvuAkYEAArrZvmp6X
+
+const bitwiseAND = (x, y) => x & y;
+
+const bitwiseOR = (x, y) => x | y;
+
+const bitwiseXOR = (x, y) => x ^= y;
+
+
+console.log(bitwiseAND(7, 12));
+console.log(bitwiseOR(7, 12));
+console.log(bitwiseXOR(7, 12));
