b79e5058944f96cbfe3ed61aa5d79f2fba63b886
jgtorrejon <joel.torrejon.mendez@gmail.com>
2021-04-16 21:54:16 -0400

    hard: add chard challenge # 1

---
+++ hard/challenge-1.js
@@ -0,0 +1,5 @@
+// Challente link: https://edabit.com/challenge/6R6gReGTGwzpwuffD
+
+const sevenBoom = list => /7/.test(list) ? "Boom!" : "there is no 7 in the array";
+
+console.log(sevenBoom([33, 68, 400, 5]));
