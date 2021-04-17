81f5981728e3b5dc9ce9f3c0d79bea9fe66b2710
jgtorrejon <joel.torrejon.mendez@gmail.com>
2021-04-16 21:21:40 -0400

    medium: add challenge # 1

---
+++ medium/challenge-1.js
@@ -0,0 +1,5 @@
+// Challente link: https://edabit.com/challenge/GLbuMfTtDWwDv2F73
+
+const countTrue = (list) => list.filter(x => x === true).length;
+
+console.log(countTrue([true, false, true, false, false]));
