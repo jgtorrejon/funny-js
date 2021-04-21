3466e0fda51289be8b11c86909f420fa3993c6d5
jgtorrejon <joel.torrejon.mendez@gmail.com>
2021-04-21 11:18:46 -0400

    expert: starting new challenge

---
+++ expert/expert-2.js
@@ -0,0 +1,12 @@
+// Challenge link: https://edabit.com/challenge/Pa2rHJ6KeRBTF28Pg
+
+const fiscalCode = (person) => { return 0; }
+
+
+
+console.log(fiscalCode({
+    name: "Matt",
+    surname: "Edabit",
+    gender: "M",
+    dob: "1/1/1900"
+  }));
